ef16533729093e2b6096ea2d1cc4b4a59c887217
Ivan23BG <ilejeune@outlook.com>
2024-02-09 08:44:28 +0100

    Update : fini tp2 + compte rendu -- manque remplacer image 22 - detection de contours

---
+++ HAI605I-Donnees-multimedia/TP-Puech/TP2/src/difference.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Ivan on 09/02/2024.
+//
+// difference.cpp : Renvoie le contour d'une image
+
+#include <stdio.h>
+#include "image_ppm.h"
+
+int main(int argc, char *argv[]) {
+    char cNomImgLue1[250], cNomImgLue2[250], cNomImgEcrite[250];
+    int nH, nW, nTaille;
+
+    if (argc != 4) {
+        printf("Usage: ImageIn1.pgm ImageIn2.pgm ImageOut.pgm\n");
+        exit(1);
+    }
+
+    sscanf(argv[1], "%s", cNomImgLue1);
+    sscanf(argv[2], "%s", cNomImgLue2);
+    sscanf(argv[3], "%s", cNomImgEcrite);
+
+    OCTET *ImgIn1, *ImgIn2, *ImgOut;
+
+    lire_nb_lignes_colonnes_image_pgm(cNomImgLue1, &nH, &nW);
+    nTaille = nH * nW;
+
+    allocation_tableau(ImgIn1, OCTET, nTaille);
+    lire_image_pgm(cNomImgLue1, ImgIn1, nH * nW);
+    allocation_tableau(ImgIn2, OCTET, nTaille);
+    lire_image_pgm(cNomImgLue2, ImgIn2, nH * nW);
+    allocation_tableau(ImgOut, OCTET, nTaille);
+
+    // initialize ImgOut to ImgIn
+    for (int i = 0; i < nTaille; i++) {
+        ImgOut[i] = ImgIn1[i];
+    }
+
+    // excluding edge cases
+    for (int i = 1; i < nH-1; i++)
+        for (int j = 1; j < nW-1; j++) {
+            // verify if both pixels are different
+            if (ImgIn1[i * nW + j] != ImgIn2[i * nW + j]) {
+                ImgOut[i * nW + j] = 0;
+            } else {
+                ImgOut[i * nW + j] = 255;
+            }
+        }
+
+    ecrire_image_pgm(cNomImgEcrite, ImgOut, nH, nW);
+    free(ImgIn1);
+    free(ImgIn2);
+    free(ImgOut);
+
+    return 1;
+}
